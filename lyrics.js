a3cc00fb6ffeb9c913212506ff0f5c06e6ea5840
Owyn Richen <owyn.richen@nike.com>
2015-10-11 14:26:41 -0700

    more cache and adding missing files

---
+++ lyrics.js
@@ -0,0 +1,386 @@
+var questions = [
+    {
+      "question" : "Ever told on a nigga?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Ever squeezed a trigger?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Ever set a nigga up?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Ever helped a brother out when he was down on his luck?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You a sap?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "You a boss player, you a mack?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Let me hold a couple dollars?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Y'all still be poppin' y'all collars?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Stock rims on a scraper?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Paint wetter than a lake?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Poodle in my blood?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Bitch, I'm a thug!?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You a loser?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Winner?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Starving?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Dinner?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You still sell dope?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Now you cleaner than a bar of Dove soap?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Got a little gouda?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Got a thumper, got a Ruger?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You in love with' the ho?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "She bringin' you the dough?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You gon' cry if she leave?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "You gon' fly overseas?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "But I never go broke",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "And I ain't gotta sell my soul",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "If you broke, you ain't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "I give a fuck if you don't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "Ugh, lazy?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Got dick that'll drive a ho crazy?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Hater?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Wanna see a player get paper?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Traitor?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Loyal to my soil, not a faker?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Sleep?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Bust moves, hella active in the streets?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Scared of the dark?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Have money, have heart?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Narc?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Shark?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Gossip like a broad?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Check a bitch like a smog?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Slippin'?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Trippin'?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Sober as a gopher?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Higher than a rollercoaster?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Star Wars?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Yoda?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Never leave the house without my strap?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Shoot a muthafucka in his nap?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Never been a sucka or a pussy?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Blowin' cookie, never coughin' like a rookie?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "But I never go broke",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "And I ain't gotta sell my soul",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "If you broke, you ain't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "I give a fuck if you don't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "Was it love at first sight?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Did she ride you like a bike?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Was it ripe?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Was her pussy tight?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Got some property?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Drive a Maserati?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "You softer than a sock?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "You solid as a rock?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Slow?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Trained to go?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Your team weak?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "You respected in the stree-neets?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Shallow?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Dineat?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Broke?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Chineat?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Not a BB or a pellet gun?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "But a long barrel base drum?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "If I get into it will run?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "I'm a give a nigga fair one?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Still live in the trap?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "You ever go back?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "Wear a wire and a camera?",
+      "answer" : "nope"
+    },
+    {
+      "question" : "Ears to the scanner?",
+      "answer" : "yep"
+    },
+    {
+      "question" : "But I never go broke",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "And I ain't gotta sell my soul",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "If you broke, you ain't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    },
+    {
+      "question" : "I give a fuck if you don't like me",
+      "answer" : "no no no"
+    },
+    {
+      "question" : "I'm a stay gettin' money",
+      "answer" : "yeah yeah yeah"
+    }
+  ]
